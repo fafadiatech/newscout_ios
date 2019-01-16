dd0c45c98db886cef8f3262052c2d9eab7428cd1
Jayashri <jayashri@apples-iMac.local>
2019-01-16 11:17:56 +0530

    added aboutUSVC

---
+++ NewsApp/Controller/AboutUsVC.swift
@@ -0,0 +1,31 @@
+//
+//  AboutUsVC.swift
+//  NewsApp
+//
+//  Created by Jayashri on 16/01/19.
+//  Copyright © 2019 Fafadia Tech. All rights reserved.
+//
+
+import UIKit
+
+class AboutUsVC: UIViewController {
+
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var txtViewAboutUs: UITextView!
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var lblAppVersion: UILabel!
+    @IBOutlet weak var lblLastUpdated: UILabel!
+    @IBOutlet weak var lblDDby: UILabel!
+    @IBOutlet weak var lblAppVersionVal: UILabel!
+    @IBOutlet weak var lblLastUpdatedVal: UILabel!
+    @IBOutlet weak var lblDDbyVal: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        lblTitle.font = 
+    }
+    
+    @IBAction func btnBackActn(_ sender: Any) {
+        
+    }
+}
