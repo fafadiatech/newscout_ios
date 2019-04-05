011922fc234310d349af90508651082f39b90917
Jayashri Minde <jayashri@fafadiatech.com>
2019-04-05 18:34:13 +0530

    added CV on ipad home VC

---
+++ NewsApp/Cell/HomeipadCVCell.swift
@@ -0,0 +1,16 @@
+//
+//  HomeipadCVCell.swift
+//  NewsApp
+//
+//  Created by Jayashree on 05/04/19.
+//  Copyright © 2019 Fafadia Tech. All rights reserved.
+//
+
+import UIKit
+
+class HomeipadCVCell: UICollectionViewCell {
+    @IBOutlet weak var imgNews: UIImageView!
+    
+    @IBOutlet weak var lblSource: UILabel!
+    @IBOutlet weak var lblTitle: UILabel!
+}
