c4998e129d0796de7c0ddb5697117f927386676f
Prasen <prasen@fafadiatech.com>
2020-03-03 11:40:33 +0530

    Initial Project structure

---
+++ NewScout/NewScout/Controllers/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewScout
+//
+//  Created by Prasen on 03/03/20.
+//  Copyright © 2020 Fafadiatech. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
